f4427e36f8be1b40e945232f9c83426c20e42d23
marinademas <marina.demas2015@gmail.com>
2021-08-12 21:06:48 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -120,3 +120,13 @@ function classToggle() {
 
 document.querySelector('.hamburger')
     .addEventListener('click', classToggle);
+
+var mobileHover = function () {
+    $('*').on('touchstart', function () {
+        $(this).trigger('hover');
+    }).on('touchend', function () {
+        $(this).trigger('hover');
+    });
+};
+
+mobileHover();
